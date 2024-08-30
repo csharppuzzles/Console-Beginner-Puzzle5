5de03b87d1d34b5511a06bce34e127f284059fad
Csharp Puzzles <162582538+csharppuzzles@users.noreply.github.com>
2024-08-30 16:00:23 +0800

    Solution Initial Commit

--- BeginnerPuzzle/Program.cs
+++ BeginnerPuzzle/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Runtime.CompilerServices;
+
 static void ExampleA()
 {
     Console.WriteLine("\nExample A");
@@ -10,8 +12,7 @@ static void ExampleA()
 
     if (input != "" && input != null)
     {
-        if (input.ToLower() == "y" || input.ToLower() == "yes")
-        {
+        if (input.ToLower() == "y" || input.ToLower() == "yes") {
 
             Console.BackgroundColor = ConsoleColor.Red;                     // Change the BACKGROUND color of the console text
             Console.WriteLine("Warning: This is Reeeeeeally important!");
@@ -40,13 +41,12 @@ static void ExampleA()
                     break;
             }
 
-        }
-        else
+        } else
         {
             Console.WriteLine("No? I don't believe you. Someone has probably done it to you though, right?");
         }
-    }
-    else
+
+    } else
     {
         Console.WriteLine("Error. You can't just enter blank space");
     }
@@ -66,7 +66,7 @@ static void ExampleA()
 //      "Thursday: 10 burpees"          (yellow text)
 //      ...
 // Remember to convert the user's input to lowercase before comparison.
-// Have a "default case" shoudl the user not enter a weekday.
+// Have a "default case" should the user not enter a weekday.
 
 
 static void PuzzleA()
@@ -74,6 +74,60 @@ static void PuzzleA()
     Console.WriteLine("\nPuzzle A");
     Console.WriteLine("~~~~~~~~~");
 
+    Console.WriteLine("What day is it?: ");
+    string? input = Console.ReadLine();
+
+    if (input != "" && input != null)
+    {
+
+        // Switch statement
+        switch (input.ToLower())
+        {
+            case "monday":
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Monday: 20 Sit ups");
+                Console.ResetColor();
+                break;
+            case "tuesday":
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Tuesday: 15 jumping jacks");
+                Console.ResetColor();
+                break;
+            case "wednesday":
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Wednesday: 20 press ups");
+                Console.ResetColor();
+                break;
+            case "thursday":
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Thursday: 10 burpees");
+                Console.ResetColor();
+                break;
+            case "friday":
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Friday: 15min jog");
+                Console.ResetColor();
+                break;
+            case "saturday":
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Saturday: 20 barbell curls");
+                Console.ResetColor();
+                break;
+            case "sunday":
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Sunday: 15 min jog");
+                Console.ResetColor();
+                break;
+            default:
+                // This will run only if they don't type a day of the week
+                Console.WriteLine($"20 bear crawls");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Error. You can't just enter blank space");
+    }
 }
 
 
@@ -92,7 +146,7 @@ static void ExampleB()
     Console.WriteLine("Choose a color: Red, Blue or Green: ");
     string? input = Console.ReadLine();
 
-    if (input != null && input != "" && input.Length > 0)
+    if(input != null && input != "" && input.Length > 0)
     {
         string inputLowerCase = input.ToLower();            // This preserves the original input variable, incase we need it later.
 
@@ -135,6 +189,52 @@ static void PuzzleB()
     Console.WriteLine("\nPuzzle B");
     Console.WriteLine("~~~~~~~~~");
 
+    string cyan = "\u001B[36m";
+    string yellow = "\u001B[33m";
+    string magenta = "\u001B[35m";
+    string reset = "\u001B[0m";
+
+    Console.WriteLine("Choose a color: Cyan, Yellow or Magenta: ");
+    string? input = Console.ReadLine();
+
+    switch (input.ToLower())
+    {
+        case "cyan":
+        case "c":
+            Console.WriteLine($"{cyan} You chose Cyan... {reset} thanks!");
+            break;
+        case "yellow":
+        case "y":
+            Console.WriteLine($"{yellow} You chose Yellow... {reset} thanks!");
+            break;
+        case "magenta":
+        case "m":
+            Console.WriteLine($"{magenta} You chose Magenta... {reset} thanks!");
+            break;
+        default:
+            Console.WriteLine("Sorry, no matching color...");
+            break;
+    }
+
+    // The 'or' keyword is also acceptable
+    /*
+    switch (input.ToLower())
+    {
+        case "cyan" or "c":
+            Console.WriteLine($"{cyan} You chose Cyan... {reset} thanks!");
+            break;
+        case "yellow" or "y":
+            Console.WriteLine($"{yellow} You chose Yellow... {reset} thanks!");
+            break;
+        case "magenta" or "m":
+            Console.WriteLine($"{magenta} You chose Magenta... {reset} thanks!");
+            break;
+        default:
+            Console.WriteLine("Sorry, no matching color...");
+            break;
+    }
+    */
+
 }
 
 
@@ -147,6 +247,7 @@ static void ExampleC()
     Console.WriteLine("Enter an operator and we will complete this sum...");
     Console.WriteLine("Enter \"+\" or \"-\" :");
     string? input = Console.ReadLine();
+
 
     switch (input)
     {
@@ -165,7 +266,6 @@ static void ExampleC()
 }
 
 
-
 // Puzzle C - Maths Switch
 // Write a program using a switch statement, that allows the user to enter 2 numbers and choose the operator + - * /
 // So the user could enter "2" then "*" then "7" in 3 separate input prompts.
@@ -175,6 +275,43 @@ static void PuzzleC()
 {
     Console.WriteLine("\nPuzzle C");
     Console.WriteLine("~~~~~~~~~");
+
+    Console.WriteLine("Enter a number :");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter \"+\" or \"-\" or \"*\" or \"/\":");
+    string? operatorInput = Console.ReadLine();
+
+    Console.WriteLine("Enter a second number :");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    switch (operatorInput)
+    {
+        case "+":
+            int addAns = num1 + num2;
+            Console.WriteLine($"{num1} + {num2} = {addAns}");
+            break;
+        case "-":
+            int minusAns = num1 - num2;
+            Console.WriteLine($"{num1} - {num2} = {minusAns}");
+            break;
+        case "*":
+            int multiplyAns = num1 * num2;
+            Console.WriteLine($"{num1} * {num2} = {multiplyAns}");
+            break;
+        case "/":
+            int divideAns = num1 / num2;
+            int remainder = num1 % num2;
+            Console.WriteLine($"{num1} / {num2} = {divideAns} remainder {remainder}");
+
+            // You could also use a double for the division.
+            double divideAns2 = (double)num1 / num2;
+            Console.WriteLine($"{num1} / {num2} = {divideAns2}");
+            break;
+        default:
+            Console.WriteLine("Error, enter only + or - or * or / ");
+        break;
+    }
 
 }
 
